f7e487059b2fc874f55c58441de21f43783c5394
pradeep2123 <35168431+pradeep2123@users.noreply.github.com>
2019-06-25 15:20:16 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -24,7 +24,7 @@ const createProduct = function(req,res){
             if(items.item == 'books'){
                 items.isImported = false
             }
-            Product.updateMany({item:product.item},{item:product.item,price:product.price,isImported:product.isImported},{upsert:false})
+            Product.updateOne({item:product.item},{item:product.item,price:product.price,isImported:product.isImported},{upsert:false})
             .then(function(updated){
                 return res.send({
                     type:"Success",
